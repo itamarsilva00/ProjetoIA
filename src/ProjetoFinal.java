b762f6d77942dcd1cbe7dbe4a6797210ce90d749
Itamar Sousa Silva <itamar.silva@meupag.com.br>
2021-07-06 21:44:00 -0300

    First commit

---
+++ src/ProjetoFinal.java
@@ -0,0 +1,12 @@
+import main.aima.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ProjetoFinal extends CSP {
+
+    public static void main(String[] args) {
+        System.out.println("Itamar aqui.....");
+    }
+}
